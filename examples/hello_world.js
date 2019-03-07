250d8c8705962c4542d2271af7faa4af051cab41
Isaac Avram <avrisaac555@gmail.com>
2019-03-06 19:49:09 -0800

    Example works

--- examples/hello_world.js
+++ examples/hello_world.js
@@ -1,8 +1,17 @@
-const {snake, bite} = require('../src/snakey');
+const http = require('http');
 const {map} = require('rxjs/operators');
+const {snake, bite} = require('../build/snakey');
 
-const server = snake((o) =>
-  bite(o, 'GET', '/').pipe(map((ctx) => ctx.makeResponse().code(200).body('Hello World!')))
-);
+const server = new http.Server();
+
+const subscriber = snake(server, (obs) => {
+  return bite(obs, 'GET', '/').pipe(map((ctx) =>
+    () => {
+      ctx.response.statusMessage = 200;
+      ctx.response.write('Hello World!');
+      ctx.response.end();
+    }
+  ));
+});
 
 server.listen(9000);
