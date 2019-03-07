0954bbb3643d1485b9b7045b9c98f40bcf51fb2c
Isaac Avram <avrisaac555@gmail.com>
2019-03-06 21:26:16 -0800

    Example

--- examples/hello_world.js
+++ examples/hello_world.js
@@ -1,17 +1,30 @@
-const http = require('http');
-const {map} = require('rxjs/operators');
-const {snake, bite} = require('../build/snakey');
+const {of} = require('rxjs');
+const {switchMap} = require('rxjs/operators');
+const {snake, bite, apply} = require('../build/snakey');
 
-const server = new http.Server();
+const app = snake(
+    snake(
+        bite('GET', '/:param'),
+        switchMap((ctx) => of(
+            () => {
+              ctx.response.statusMessage = 200;
+              ctx.response.write(JSON.stringify(ctx.pathMatch));
+              ctx.response.end();
+            }
+        ))
+    ),
+    snake(
+        bite('GET', '/'),
+        switchMap((ctx) => of(
+            () => {
+              ctx.response.statusMessage = 200;
+              ctx.response.write('Hello World!');
+              ctx.response.end();
+            }
+        )),
+    )
+);
 
-const subscriber = snake(server, (obs) => {
-  return bite(obs, 'GET', '/').pipe(map((ctx) =>
-    () => {
-      ctx.response.statusMessage = 200;
-      ctx.response.write('Hello World!');
-      ctx.response.end();
-    }
-  ));
-});
+const {server, subscribers} = apply({'streams': app});
 
 server.listen(9000);
