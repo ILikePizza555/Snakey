916369d34323431dd815a6a30f862d164c094872
Isaac Avram <avrisaac555@gmail.com>
2019-03-11 20:28:31 -0700

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
 /* eslint require-jsdoc: 0 */
-const {series, dest} = require('gulp');
-const {createProject} = require('gulp-typescript');
+const {series, dest, src} = require('gulp');
+const ts = require('gulp-typescript');
+const path = require('path');
+const mocha = require('gulp-mocha');
 
-const tsProject = createProject('tsconfig.json');
+const tsProject = ts.createProject('tsconfig.json');
+
+const LIB_DEST = 'dist/lib';
+const TEST_DEST = 'dist/test';
 
 function buildLib() {
   return tsProject.src()
       .pipe(tsProject())
       .js
-      .pipe(dest('build'));
+      .pipe(dest(LIB_DEST));
+}
+
+function buildTest() {
+  return src('./test/*.test.ts')
+      .pipe(ts())
+      .js
+      .pipe(dest(TEST_DEST));
+}
+
+function runUnitTests() {
+  return src(path.join(TEST_DEST, '*.js'))
+      .pipe(mocha({reporter: 'nyan'}));
 }
 
 exports.buildLib = buildLib;
+exports.buildTest = buildTest;
+exports.runTests = series(buildTest, runUnitTests);
