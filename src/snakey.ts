f355dd0190df89df75028ce367ba198dd2cce984
Isaac Avram <avrisaac555@gmail.com>
2019-03-12 15:18:30 -0700

    Revert "Made Snake an interface and added concrete classes"
    This reverts commit 67e7e159e8a94adce81cad23941a92149288b508.

--- src/snakey.ts
+++ src/snakey.ts
@@ -1,9 +1,8 @@
-import {Observable, Observer, fromEvent, OperatorFunction, Subscription, Operator, UnaryFunction} from 'rxjs';
+import {Observable, Observer, fromEvent, OperatorFunction, Subscription} from 'rxjs';
 import * as rxop from 'rxjs/operators';
 import {Server, IncomingMessage, ServerResponse} from 'http';
 import {matchRegex, matchPathPattern, PathMatch} from './match';
 import {parse, URIComponents} from 'uri-js';
-import { compose } from './utils';
 
 export type PathPattern = string | RegExp;
 
@@ -64,58 +63,7 @@ export function bite(verb: string, pathPattern: PathPattern) {
   );
 }
 
-/**
- * An iterable and immutable collection of OperatorFunctions.
- * The first OperatorFunction must take A as a parameter, and the last
- * OperatorFunction must return B.
- * 
- * The composition of all OperatorFunctions must be a function which takes an A and returns a B.
- */
-export interface Snake<A, B> {
-  readonly first: OperatorFunction<A, any>;
-  readonly last: OperatorFunction<any, B>;
-
-  readonly length: number;
-
-  push<T>(op: OperatorFunction<B, T>): Snake<A, T>;
-  compose(): OperatorFunction<A, B>;
-}
-
-class ListSnake<A, B> implements Snake<A, B> {
-  constructor(readonly first: OperatorFunction<A, any>,
-              readonly center: OperatorFunction<any, any>[],
-              readonly last: OperatorFunction<any, B>) {
-    Object.freeze(this);
-  }
-
-  get length(): number {
-    return 2 + this.center.length;
-  }
-
-  push<T>(op: OperatorFunction<B, T>): Snake<A, T> {
-    return new ListSnake(this.first, this.center.concat(this.last), op);
-  }
-
-  compose(): OperatorFunction<A, B> {
-    return compose(this.first, ...this.center, this.last)
-  }
-}
-
-class SingleSnake<A, B> implements Snake<A, B> {
-  constructor(readonly single: OperatorFunction<A, B>) {
-    Object.freeze(this);
-  }
-
-  get first() {return this.single;}
-  get last() {return this.single;}
-  get length() {return 1;}
-
-  push<T>(op: OperatorFunction<B, T>): Snake<A, T> {
-    return new ListSnake(this.single, [], op);
-  }
-
-  compose() {return this.single;}
-}
+export type Snake<T, R> = OperatorFunction<T, R>[]
 
 export type SnakeResult = {
   server: Server,
@@ -123,7 +71,7 @@ export type SnakeResult = {
   subscribers: Subscription[]
 }
 
-export function applySnakes(snake: Snake<Context, Responder>[], 
+export function applySnakes(snake: Snake<any, any>[], 
                             server: Server = new Server(),
                             observer = new ResponderObserver): SnakeResult {
   const obs = fromEvent<[IncomingMessage, ServerResponse]>(server, 'request')
@@ -131,7 +79,7 @@ export function applySnakes(snake: Snake<Context, Responder>[],
       rxop.map(([req, res]) => new Context(req, res))
     );
 
-  const streams = snake.map(s => obs.pipe(s.compose()));
+  const streams = snake.map((s) => s.reduce((acc, cur) => acc.pipe(cur), obs));
   
   return {
     'server': server,
