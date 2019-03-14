c02f7ff05bcc0437c0291008cd1fab1486580973
Isaac Avram <avrisaac555@gmail.com>
2019-03-14 16:46:58 -0700

    Responder turned into a class

--- src/response.ts
+++ src/response.ts
@@ -1,11 +1,34 @@
 import { ServerResponse } from "http";
 import { Observer } from "rxjs";
+import { isString } from "util";
+
+export type HeaderMap = {[name: string]: string | number | string[]};
 
 /**
- * Function interface that consumes a ServerResponse to send data to the client.
+ * A Responder is an object that collects the information needed to construct a response
+ * to the client, and provides a method which sends the response.
+ * 
  */
-export interface Responder {
-    (res: ServerResponse): void;
+export class Responder {
+    constructor(readonly resObj: ServerResponse,
+                readonly body: string | {toString(): string} = "",
+                readonly status: number = 200,
+                readonly headers: HeaderMap = {},
+                readonly encoding: string = "utf-8",
+                readonly endResponse = true) {
+    }
+
+    respond() {
+        this.resObj.writeHead(this.status, this.headers);
+
+        const chunk: string = (isString(this.body)) ? this.body : this.body.toString();
+
+        if(this.endResponse) {
+            this.resObj.end(chunk, this.encoding);
+        } else {
+            this.resObj.write(chunk, this.encoding);
+        }
+    }
 }
 
 /**
