2db722d9e9b7e157799777d2d4bad282fa866eb1
Isaac Avram <avrisaac555@gmail.com>
2019-03-13 21:45:27 -0700

    Added functions to snake.ts

--- src/snake.ts
+++ src/snake.ts
@@ -1,4 +1,4 @@
-import { OperatorFunction } from "rxjs";
+import { Observable, OperatorFunction, UnaryFunction } from "rxjs";
 
 /**
  * A Snake is an OperatorFunction that can be chained with another OperatorFunction
@@ -6,4 +6,25 @@ import { OperatorFunction } from "rxjs";
  */
 export interface Snake<T, R> extends OperatorFunction<T, R> {
     chain<N>(op: OperatorFunction<R, N>) : Snake<T, N>
+}
+
+function compose<A, B, C>(f1: UnaryFunction<A, B>, f2: UnaryFunction<B, C>): UnaryFunction<A, C> {
+    return (a: A) => f2(f1(a));
+}
+
+/**
+ * Takes an OperatorFunction and transforms it into a Snake.
+ * @param op 
+ */
+export function tfSnake<T, R>(op: OperatorFunction<T, R>): Snake<T, R> {
+    const rv: Snake<T, R> = (o: Observable<T>) => op(o);
+    rv.chain = <N>(f: OperatorFunction<R, N>) => tfSnake(compose(this, f));
+    return rv;
+}
+
+/**
+ * Instantiates an 'indentity' snake.
+ */
+export function iSnake<T>(): Snake<T, T> {
+    return tfSnake((o: Observable<T>) => o);
 }
