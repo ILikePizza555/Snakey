582d0193fc31187652e170454ba6278014f8a911
Isaac Avram <avrisaac555@gmail.com>
2019-03-14 16:53:34 -0700

    Fixed snake definition

--- src/snake.ts
+++ src/snake.ts
@@ -1,4 +1,5 @@
 import { Observable, OperatorFunction, UnaryFunction } from "rxjs";
+import { isNullOrUndefined } from "util";
 
 /**
  * A Snake is an OperatorFunction that can be chained with another OperatorFunction
@@ -31,7 +32,6 @@ export function tfSnake<T, R>(op: OperatorFunction<T, R>): Snake<T, R> {
  * Creates a new snake from the OperatorFunction. If no operator function is passed,
  * then an "indentity Snake" is created.
  */
-export function snake<T, R>(op: OperatorFunction<T, R>): Snake<T, R>;
-export function snake<T>(op: OperatorFunction<T, T> = (t: Observable<T>) => t): Snake<T, T> {
+export function snake<T, R = T>(op: OperatorFunction<T, R|T> = (t: Observable<T>) => t): Snake<T, R|T> {
     return tfSnake(op);
 }
