b9a39d8225a5e3302525e0f943705dc0a5970ada
Isaac Avram <avrisaac555@gmail.com>
2019-03-17 13:16:53 -0700

    Revert "Snake is now a function which returns Either."
    This reverts commit 59c26409659905f3296f90c96d5a68068a42bcc3.

--- src/snake.ts
+++ src/snake.ts
@@ -2,81 +2,36 @@ import { Observable, OperatorFunction, UnaryFunction } from "rxjs";
 import { Either, Right, Left, isRight } from "./Either";
 
 /**
- * A Snake is a unique operator function which creates a disjoint stream. It is capable of composing itself with other
- * OperatorFunctions through the use of `map` or other `Snakes` through the use of `chain`.
+ * A Snake is an OperatorFunction that can be chained with another OperatorFunction
+ * to produce a new Snake with a different return value.
  * 
  * Because snakes build individually on each other, this creates a type-safe method
  * of passing in a series of operator functions.
+ * 
+ * ... This is just a Functor, isn't it?
  */
-export interface Snake<I, E, R> {
-    (o: Observable<I>): Either<Observable<E>, Observable<R>>
-
-    map<N>(op: OperatorFunction<R, N>) : Snake<I, E, N>
-    chain<N>(s: Snake<R, E, N>): Snake<I, E, N>
+export interface Snake<T, R> extends OperatorFunction<T, R> {
+    chain<N>(op: OperatorFunction<R, N>) : Snake<T, N>
 }
 
-export type SnakeFunc<I, E, R> = (i: Observable<I>) => Either<Observable<E>, Observable<R>>;
-
-/**
- * Lifts a normal function matching Snake to a real Snake.
- */
-export function liftSnake<I, E, R>(f: SnakeFunc<I, E, R>): Snake<I, E, R> {
-    const rv: Snake<I, E, R> = (o: Observable<I>) => f(o);
-
-    rv.map = <N>(f: OperatorFunction<R, N>) => 
-        liftSnake((i: Observable<I>) => rv(i).map(f));
-
-    rv.chain = <N>(s: Snake<R, E, N>) => 
-        liftSnake((i: Observable<I>) => rv(i).chain(s));
-
-    return rv;
+function compose<A, B, C>(f1: UnaryFunction<A, B>, f2: UnaryFunction<B, C>): UnaryFunction<A, C> {
+    return (a: A) => f2(f1(a));
 }
 
 /**
  * Takes an OperatorFunction and transforms it into a Snake.
  * @param op 
  */
-export function tfSnake<I, E, R>(op: OperatorFunction<I, R>): Snake<I, E, R> {
-    const rv: Snake<I, E, R> = (o: Observable<I>) => new Right(op(o));
-
-    rv.map = <N>(f: OperatorFunction<R, N>) => 
-        liftSnake((i: Observable<I>) => rv(i).map(f));
-
-    rv.chain = <N>(s: Snake<R, E, N>) =>
-        liftSnake((i: Observable<I>) => rv(i).chain(s));
-    
+export function tfSnake<T, R>(op: OperatorFunction<T, R>): Snake<T, R> {
+    const rv: Snake<T, R> = (o: Observable<T>) => op(o);
+    rv.chain = <N>(f: OperatorFunction<R, N>) => tfSnake(compose(rv, f));
     return rv;
 }
 
 /**
- * Creates a new "identity snake".
+ * Creates a new snake from the OperatorFunction. If no operator function is passed,
+ * then an "indentity Snake" is created.
  */
-export function snake<I, E, R>(r: Observable<R>): Snake<I, E, R> {
-    return liftSnake(() => new Right(r));
-}
-
-/**
- * Creates a new "error snake".
- */
-export function errorSnake<I, E, R>(e: Observable<E>): Snake<I, E, R> {
-    return liftSnake(() => new Left(e));
-}
-
-/**
- * Takes two snakes and executes the first one. If a right value is returned, the value is returned.
- * Otherwise, the value of the second snake is returned.
- * @param s1 
- * @param s2 
- */
-export function concatSnake<I, E, R>(s1: Snake<I, E, R>, s2: Snake<I, E, R>): Snake<I, E, R> {
-    return liftSnake((i: Observable<I>) => {
-        const r1 = s1(i);
-
-        if(isRight(r1)) { return r1; }
-        else {return s2(i); }
-    });
-}
-
-export function concatAllSnakes<I, E, R>(snakes: Snake<I, E, R>[]): Snake<I, E, R> {
-    return snakes.reduce((prev, cur) => concatSnake(cur, prev))
+export function snake<T, R = T>(op: OperatorFunction<T, R|T> = (t: Observable<T>) => t): Snake<T, R|T> {
+    return tfSnake(op);
 }
